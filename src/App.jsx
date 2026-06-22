cfb51a5605e26fe28defada0482b75962d0c588d
krishnakeshab-banik <krishna.keshab.banik@gmail.com>
2026-06-22 18:30:52 +0530

    Login Page Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import login from './pages/login.jsx'
-import Feed from './pages/feed.jsx'
-import Messages from './pages/messages.jsx'
-import Chatbox from './components/chatbox.jsx'
-import Connections from './pages/connections.jsx'
-import Discover from './pages/discover.jsx'
-import Profile from './pages/profile.jsx'
-import CreatePost from './components/createPost.jsx'
+import Login from './pages/Login.jsx'
+import Feed from './pages/Feed.jsx'
+import Messages from './pages/Messages.jsx'
+import Chatbox from './pages/Chatbox.jsx'
+import Connections from './pages/Connections.jsx'
+import Discover from './pages/Discover.jsx'
+import Profile from './pages/Profile.jsx'
+import CreatePost from './pages/CreatePost.jsx'
 
 const App = () => {
   return (
     <>
   <Routes>
-    <Route path='/' element={<login />} >
-     <Route index element={<Feed/>}>
+    <Route path='/' element={<Login />} >
+     <Route index element={<Feed/>}/>
      <Route path='messages/' element={<Messages/>}/>
      <Route path='messages/:userId=' element={<Chatbox/>}/>
      <Route path='connections' element={<Connections/>}/>
      <Route path='discover' element={<Discover/>}/>
-     <Route path='profile/:profileId' element={<Profile/>}/
+     <Route path='profile/:profileId' element={<Profile/>}/>
      <Route path='create-post' element={<CreatePost/>}/>
-
-     =</Route>
     </Route>
   </Routes>
     </>
