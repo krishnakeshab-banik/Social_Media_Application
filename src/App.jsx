4693654e77776266541ac2db98a3603f967da190
krishnakeshab-banik <krishna.keshab.banik@gmail.com>
2026-07-06 04:09:42 +0530

    Story section complete

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,15 @@ import Discover from './pages/Discover.jsx'
 import Profile from './pages/Profile.jsx'
 import CreatePost from './pages/CreatePost.jsx'
 import {useUser} from '@clerk/clerk-react'
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
   const {user} = useUser()
   return (
     <>
+  <Toaster />
   <Routes>
-    <Route path='/' element={ !user ? <Login /> : <Layout/>}> // when te user is not logged in, show the login page
+    <Route path='/' element={ !user ? <Login /> : <Layout/>}> 
      <Route index element={<Feed/>}/>
      <Route path='messages/' element={<Messages/>}/>
      <Route path='messages/:userId=' element={<Chatbox/>}/>
